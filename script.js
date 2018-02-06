5f863389c88af6209e4a806a011e0be92756c62b
LukaszWiszniewski <wisznu07@gmail.com>
2018-02-06 20:53:53 +0100

    Added movie variable

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-var img= new Image;
+var movie = React.createClass({
+    propTypes: {
+        id: React.PropTypes.object.isRequired,
+    },
+    render: function () {
+        return (
+            React.createElement('h2', {}, movie.title),
+                React.createElement('p', {}, movie.desc),
+            React.createElement('img', {src: movie.poster})
+    )
+    }
+});
+
+var img = new Image;
 
 var movies = [
     {
-        id:1,
+        id: 1,
         title: 'Harry Potter',
         desc: 'Movie about magician',
-        poster:'./posters/harry.jpg'
+        poster: './posters/harry.jpg'
     },
     {
-        id:2,
+        id: 2,
         title: 'The Lion King',
         desc: 'Film about wild cat',
-        poster:'./posters/lion.jpg'
+        poster: './posters/lion.jpg'
     },
     {
-        id:3,
+        id: 3,
         title: 'The Ilusionist',
-        desc:'In turn-of-the-century Vienna, a magician uses his abilities to secure the love of a woman far above his social standing.',
-        poster:'./posters/ilusionist.jpg'
+        desc: 'In turn-of-the-century Vienna, a magician uses his abilities to secure the love of a woman far above his social standing.',
+        poster: './posters/ilusionist.jpg'
     },
     {
-        id:4,
+        id: 4,
         title: 'Solo',
         desc: 'During an adventure into a dark criminal underworld, Han Solo meets his future copilot Chewbacca and encounters Lando Calrissian years before joining the Rebellion.',
-        poster:'./posters/solo.jpg'
+        poster: './posters/solo.jpg'
     }
 ];
 var moviesElements = movies.map(function (movie) {
     return React.createElement('li', {key: movie.id},
         React.createElement('h2', {}, movie.title),
         React.createElement('p', {}, movie.desc)),
-        React.createElement('img',{src:movie.poster});
+        React.createElement('img', {src: movie.poster});
 });
 var element =
     React.createElement('div', {},
